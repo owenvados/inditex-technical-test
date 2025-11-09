f88aed0907232e0bfe926f9e4120c06121c54fa9
Francisco Jiménez <francisco@inlabdigital.com>
2025-11-10 00:54:19 +0100

    chore: consolidate global styles entry

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app/App';
-import './styles/reset.css';
-import './styles/global.css';
+import './styles/index.css';
 
 const container = document.getElementById('root');
 
