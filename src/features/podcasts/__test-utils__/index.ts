659007177e2efed8c2f6664b8c4aa14297927d30
Francisco Jiménez <francisco@inlabdigital.com>
2025-11-12 23:04:22 +0100

    refactor: move test mocks to __test-utils__ directory
    - Move podcastMocks.ts from __mocks__ to __test-utils__
    - Create index.ts for easier imports
    - Update tests to use new location (@podcasts/__test-utils__)
    - Better organization: test utilities are not Jest auto-mocks

---
+++ src/features/podcasts/__test-utils__/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Test utilities and fixtures for podcasts feature.
+ *
+ * This module exports mock data and test helpers that can be used
+ * across different test files in the podcasts feature.
+ */
+export * from './podcastMocks';
